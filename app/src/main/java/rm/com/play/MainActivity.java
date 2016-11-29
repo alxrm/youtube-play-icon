a7527a9abc3915aceab1e137f1015f44f13ee5b1
alex <rupta3@gmail.com>
2016-11-29 23:47:24 +0300

    Initial commit

---
+++ app/src/main/java/rm/com/play/MainActivity.java
@@ -0,0 +1,13 @@
+package rm.com.play;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
